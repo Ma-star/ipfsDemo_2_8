09bebf456843395566cb470bb9fa8cef5e96b45c
bill080307 <bill080307@live.cn>
2018-08-11 20:21:42 +0800

    添加图片的两个属性：标题和描述

--- website/src/components/Upload.vue
+++ website/src/components/Upload.vue
@@ -3,6 +3,12 @@
     <b-container class="bv-example-row">
       <b-form-file v-model="file" :state="Boolean(file)" placeholder="选择图片"></b-form-file>
       <div class="mt-3">已选图片: {{file && file.name}}</div>
+      <b-form-input v-model="title"
+                  type="text"
+                  placeholder="输入标题"></b-form-input>
+      <b-form-input v-model="description"
+                  type="text"
+                  placeholder="输入描述"></b-form-input>
       <b-button type="submit" variant="primary">上传</b-button>
     </b-form>
     </b-container>
@@ -13,7 +19,9 @@
 export default {
   data () {
     return {
-      file: null
+      file: null,
+      title: '',
+      description: '',
     }
   },
   methods: {
